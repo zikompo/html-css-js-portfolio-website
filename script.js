c3615da2d787027cce9e7b675686e9a41d9f2231
Zikora Chinedu <99816857+zikompo@users.noreply.github.com>
2024-12-26 21:37:32 +0100

    Added projects to projects section.

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function openVideoModal() {
+    document.getElementById("videoModal").style.display = "block";
+  }
+  
+  function closeVideoModal() {
+    document.getElementById("videoModal").style.display = "none";
+  }
+  
+  window.onclick = function(event) {
+    const modal = document.getElementById("videoModal");
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
